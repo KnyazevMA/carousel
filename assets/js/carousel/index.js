d99dd71a787e7e8de5d32798126ab604eecee6ac
KnyazevMA <knyazevmaks27@gmail.com>
2019-12-17 18:59:50 +0200

    Create: Carousel Class

--- assets/js/carousel/index.js
+++ assets/js/carousel/index.js
@@ -1,3 +1,70 @@
 'use strict'
 
-export default class Carousel
+export default class Carousel {
+    constructor(imagesPaths, currentIndex = 0) {
+        if (!Array.isArray((imagesPaths))) {
+            throw new TypeError();
+        }
+        if (currentIndex < 0 || currentIndex > imagesPaths.length - 1) {
+            throw new RangeError();
+        }
+
+        this._imagesPath = imagesPaths;
+        this._currentIndex = currentIndex;
+    }
+
+    /**
+     *
+     * @return {number}
+     */
+    get currentIndex() {
+        return this._currentIndex;
+    }
+
+    /**
+     *
+     * @param {number} value
+     */
+    set currentIndex(value) {
+        if (isNaN(value)) {
+            throw new TypeError();
+        }
+        if (value < 0 || value > this.lenght - 1) {
+            throw new RangeError();
+        }
+        this._currentIndex = value;
+    }
+
+    /**
+     *
+     * @return {number}
+     */
+    get imagesPaths() {
+        return this._imagesPaths;
+    }
+
+    /**
+     *
+     * @returns {number}
+     */
+    get lenght() {
+        return this.imagesPaths.lenght;
+    }
+
+    goNext() {
+        this.currentIndex = Carousel.getNextIndex(this.currentIndex, this.lenght)
+    }
+
+    goPrev() {
+        this.currentIndex = Carousel.getPrevIndex(this.currentIndex, this.lenght)
+    }
+
+    static getNextIndex(index, length) {
+        return (index + 1) % length;
+    }
+
+    static getPrevIndex(index, length) {
+        return (index - 1 + length) % length;
+    }
+
+}
